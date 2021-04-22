b9ead693095eaf8203efa1c3da95ea347c46624c
ferhammaren <fernanda.meza.lopez@gmail.com>
2021-04-22 11:48:15 -0700

    Add project files.

---
+++ Models/Payment.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserPayments.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string concept { get; set; }
+        public int userId { get; set; }
+
+        public static Payment getPayment (int id)
+        {
+            Payment obj=new Payment();
+            
+            return obj;
+        }
+
+        public static bool EditPayment(int id, decimal Amount)
+        {
+            if (getPayment(id) != null)
+            {
+                Payment obj = getPayment(id);
+                obj.Amount = Amount;
+                obj.PaymentDate = DateTime.Now;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool DeletePayment(int id, decimal Amount)
+        {
+            if (getPayment(id) != null)
+            {
+                Payment obj = getPayment(id);
+               
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ Models/PaymentTestContext.cs
@@ -0,0 +1,76 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserPayments.Models
+{
+    public class PaymentTestContext
+    {
+        public string ConnectionString { get; set; }
+
+        public PaymentTestContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public Payment getPayment(int id)
+        {
+            Payment obj = new Payment();
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "GET_PAYMENT";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@paymentID",id);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        obj.PaymentId = id;
+                        obj.PaymentDate = Convert.ToDateTime(reader["date"]);
+                        obj.Amount = Convert.ToDecimal(reader["amount"]);
+                        obj.concept = reader["concept"].ToString();
+                    }
+                }
+            }
+            return obj;
+        }
+
+
+        public List<Payment> getPayments(int id)
+        {
+            List<Payment> obj = new List<Payment>();
+            
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "getUserPayments";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("userId", id);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Payment obj1 = new Payment();
+                        obj1.PaymentId = id;
+                        obj1.PaymentDate = Convert.ToDateTime(reader["date"]);
+                        obj1.Amount = Convert.ToDecimal(reader["amount"]);
+                        obj1.concept = reader["concept"].ToString();
+                        obj.Add(obj1);
+                    }
+                }
+            }
+            return obj;
+        }
+
+
+    }
+}
